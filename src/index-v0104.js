7cb4891832ff44e8d3245f808885b3bee3c3f3c0
Brandi Neal <nealb3@nku.edu>
2026-09-07 16:43:08 -0400

    Load Week 03 ShowRunner session seed

--- src/index-v0104.js
+++ src/index-v0104.js
@@ -44,7 +44,7 @@ export default {
     html = addBefore(html, '</head>', '<link rel="stylesheet" href="/v0109-icons.css">'); html = addBefore(html, '</body>', '<script src="/v0109-icons.js"></script>');
     if (path === '/builder') {
       for (const f of ['v0104-polish.css','v0105-editor.css','v0106-guide.css','v0108-workspace.css','v01010-navigation.css','v01011-simplify.css','v01013-layered.css','v01017-share.css']) html=addBefore(html,'</head>',`<link rel="stylesheet" href="/${f}">`);
-      for (const f of ['week2-seeds.js','week2-v0107.js','week2-v01015.js','week2-v01020.js','v0105-editor.js','v0106-guide.js','v0108-workspace.js','v01010-navigation.js','v01011-simplify.js','v01012-recovery.js','v01013-layered.js','v01017-share.js','v01019-builder.js']) html=addBefore(html,'</body>',`<script src="/${f}"></script>`);
+      for (const f of ['week2-seeds.js','week3-seeds.js','week2-v0107.js','week2-v01015.js','week2-v01020.js','v0105-editor.js','v0106-guide.js','v0108-workspace.js','v01010-navigation.js','v01011-simplify.js','v01012-recovery.js','v01013-layered.js','v01017-share.js','v01019-builder.js']) html=addBefore(html,'</body>',`<script src="/${f}"></script>`);
     }
     if(path==='/student'){
       html=addBefore(html,'</head>','<link rel="stylesheet" href="/v01021-research.css">');
